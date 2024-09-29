7ff1d13a469b0bd179ac2ef53d86b77a9ae35eee
Lucas Bueno Cesario <lucas.cesario@luizalabs.com>
2024-09-29 18:45:36 -0300

    deletes exportation unused code

--- src/CanvasManager/screenObject.py
+++ src/CanvasManager/screenObject.py
@@ -18,16 +18,3 @@ class ScreenObject:
         self.normalized_coords = normalized_coordinate_transform(
             self.world_coords, window
         )
-
-    def obj_string(self):
-        obj_str = f"o {self.name}\n"
-        for x, y in self.world_coords:
-            obj_str += f"v {x} {y} 0\n"
-
-        if self.type in ["line", "polygon"]:
-            num_vertices = len(self.world_coords)
-            for i in range(num_vertices):
-                next_i = (i + 1) % num_vertices
-                obj_str += f"l {i+1} {next_i+1}\n"
-
-        return obj_str
